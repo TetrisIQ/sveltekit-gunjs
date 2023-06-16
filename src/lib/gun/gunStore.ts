166619fd8bc178e87016e7fa76f3d9dfd3a7bd5f
TetrisIQ <dev@tetrisiq.de>
2023-06-16 18:43:58 +0200

    fix: server-side gun

--- src/lib/gun/gunStore.ts
+++ src/lib/gun/gunStore.ts
@@ -1,7 +1,10 @@
 import { writable } from 'svelte/store';
-import GUN, { SEA, type IGun } from 'gun';
+import GUN from 'gun';
 import 'gun/sea';
+import { browser } from '$app/environment';
 
-export const gun = new GUN({ peers: ['http://127.0.0.1:8765/gun'] });
+export const gun = browser
+	? new GUN({ peers: [`http://${window.location.host}/gun`] })
+	: global.gun;
 
 export const messages = writable<Array<string>>([]);
